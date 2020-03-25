d4c94826cd6f42b589e6d32297cde5c1d85a519f
“Lorenzo <dentis.lorenzo@educ.di.unito.it>
2020-03-25 10:08:29 +0100

    created new tests

--- Esercizio1/test/sorting_test.c
+++ Esercizio1/test/sorting_test.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int numberList[5]={1,5,3,2,7};
+
 static int int_comparer(int *a, int *b) {
   return *a-*b;
 }
@@ -17,7 +19,6 @@ void empty_array(){
 
 void casual_array(){
 	int ** array;
-	int numberList[5]={1,5,3,2,7};
 
 	array=malloc(sizeof(int *)*5);
 	for (int i = 0; i < 5; ++i){
@@ -75,6 +76,67 @@ void equal_array(){
 		}
 }
 
+void ordered_array(){
+  int ** array;
+
+  array=malloc(sizeof(int *)*5);
+  for (int i = 0; i < 5; ++i){
+    array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
+  
+  insertion_sort((void*)array,5,(cmpFunction)int_comparer);
+  TEST_ASSERT_EQUAL(1,*array[0]);
+  TEST_ASSERT_EQUAL(2,*array[1]);
+  TEST_ASSERT_EQUAL(3,*array[2]);
+  TEST_ASSERT_EQUAL(5,*array[3]);
+  TEST_ASSERT_EQUAL(7,*array[4]);
+
+  array=malloc(sizeof(int *)*5);
+  for (int i = 0; i < 5; ++i){
+    array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
+  
+  quick_sort((void*)array,5,(cmpFunction)int_comparer);
+  TEST_ASSERT_EQUAL(1,*array[0]);
+  TEST_ASSERT_EQUAL(2,*array[1]);
+  TEST_ASSERT_EQUAL(3,*array[2]);
+  TEST_ASSERT_EQUAL(5,*array[3]);
+  TEST_ASSERT_EQUAL(7,*array[4]);
+}
+
+
+void inverted_array(){
+  int ** array;
+
+  array=malloc(sizeof(int *)*5);
+  for (int i = 0; i < 5; ++i){
+    array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
+  
+  insertion_sort((void*)array,5,(cmpFunction)int_comparer);
+  TEST_ASSERT_EQUAL(1,*array[0]);
+  TEST_ASSERT_EQUAL(2,*array[1]);
+  TEST_ASSERT_EQUAL(3,*array[2]);
+  TEST_ASSERT_EQUAL(5,*array[3]);
+  TEST_ASSERT_EQUAL(7,*array[4]);
+
+  array=malloc(sizeof(int *)*5);
+  for (int i = 0; i < 5; ++i){
+    array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
+  
+  quick_sort((void*)array,5,(cmpFunction)int_comparer);
+  TEST_ASSERT_EQUAL(1,*array[0]);
+  TEST_ASSERT_EQUAL(2,*array[1]);
+  TEST_ASSERT_EQUAL(3,*array[2]);
+  TEST_ASSERT_EQUAL(5,*array[3]);
+  TEST_ASSERT_EQUAL(7,*array[4]);
+
+}
 
 int main(){
 	UNITY_BEGIN();
@@ -82,6 +144,8 @@ int main(){
 	RUN_TEST(empty_array);
 	RUN_TEST(casual_array);
 	RUN_TEST(equal_array);
+  RUN_TEST(ordered_array);
+  RUN_TEST(inverted_array);
 
 	UNITY_END();
 }
