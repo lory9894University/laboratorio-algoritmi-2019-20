bcca3963f60d4f54c6516ae9659385a41020601c
“Lorenzo <dentis.lorenzo@educ.di.unito.it>
2020-03-27 00:06:45 +0100

    written ExampleApp

--- Esercizio2/src/exampleapp/ExampleApp.java
+++ Esercizio2/src/exampleapp/ExampleApp.java
@@ -2,10 +2,67 @@ package exampleapp;
 
 import editdistance.EditDistance;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class ExampleApp {
+  ArrayList<String> toCorrect = null;
+  private ArrayList<String> dictionary = null;
+
+  public ExampleApp(String toBeCorrected, String dictionary) {
+
+    try (BufferedReader in = new BufferedReader(new FileReader(toBeCorrected))) {
+      toCorrect = in.lines().map(x -> x.replaceAll("\\p{Punct}", "").toLowerCase())
+          .flatMap((a) -> Stream.of(a.split(" ")))
+          .collect(Collectors.toCollection(ArrayList::new));
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+
+    try (BufferedReader in = new BufferedReader(new FileReader(dictionary))) {
+      this.dictionary = in.lines().collect(Collectors.toCollection(ArrayList::new));
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+
+  }
+
   public static void main(String[] args) {
-    int distance;
-    distance = EditDistance.editDistanceRecursive("ciao", "caio");
-    System.out.println(distance);
+    ExampleApp autocorrect = new ExampleApp("documents/correctme.txt", "documents/smaller.txt");
+    autocorrect.getCorrection().entrySet().stream().filter(entry -> entry.getValue().get(0).compareTo(entry.getKey()) != 0).forEach(System.out::println);
+  }
+
+  private ArrayList<String> fixWord(String word) {
+    ArrayList<String> alternatives = new ArrayList<>();
+    int minEditDistance = Integer.MAX_VALUE;
+    int localEditDistance = 0;
+
+    for (String correctWord : dictionary) {
+      localEditDistance = EditDistance.editDistanceRecursive(word, correctWord); //TODO: questo è la versione ricorsiva
+      if (localEditDistance < minEditDistance) {
+        alternatives.clear();
+        minEditDistance = localEditDistance;
+      }
+      if (localEditDistance == minEditDistance)
+        alternatives.add(correctWord);
+    }
+
+    return alternatives;
+  }
+
+  public HashMap<String, ArrayList<String>> getCorrection() {
+    HashMap<String, ArrayList<String>> correction = new HashMap<>();
+
+    for (String word : toCorrect)
+      correction.put(word, fixWord(word));
+    return correction;
   }
 }
