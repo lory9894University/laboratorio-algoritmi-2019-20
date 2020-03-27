10e8079ba3371aee2031d24b780352e886beda81
“Lorenzo <dentis.lorenzo@educ.di.unito.it>
2020-03-27 11:17:03 +0100

    wrote javadoc

--- Esercizio2/src/exampleapp/ExampleApp.java
+++ Esercizio2/src/exampleapp/ExampleApp.java
@@ -14,8 +14,13 @@ public class ExampleApp {
   ArrayList<String> toCorrect = null;
   private ArrayList<String> dictionary = null;
 
+  /**
+   * read from two files and saves in the new generated object a dictionary and a list of word to analyze
+   *
+   * @param toBeCorrected pathname of the file that needs to be corrected
+   * @param dictionary    pathname of the dictionary file
+   */
   public ExampleApp(String toBeCorrected, String dictionary) {
-
     try (BufferedReader in = new BufferedReader(new FileReader(toBeCorrected))) {
       toCorrect = in.lines().map(x -> x.replaceAll("\\p{Punct}", "").toLowerCase())
           .flatMap((a) -> Stream.of(a.split(" ")))
@@ -37,6 +42,12 @@ public class ExampleApp {
     autocorrect.getCorrection().entrySet().stream().filter(entry -> entry.getValue().get(0).compareTo(entry.getKey()) != 0).forEach(System.out::println);
   }
 
+  /**
+   * confront a word against the dictionary to find all the possible spelling correction
+   *
+   * @param word words thant needs to be grammar checked
+   * @return an ArrayList of possible correction
+   */
   private ArrayList<String> fixWord(String word) {
     ArrayList<String> alternatives = new ArrayList<>();
     int minEditDistance = Integer.MAX_VALUE;
@@ -55,6 +66,11 @@ public class ExampleApp {
     return alternatives;
   }
 
+  /**
+   * confronts the two Array of word saved during Construction fixing all the spelling mistake.
+   *
+   * @return a map having the word as Key and a Collection of possible fix as value
+   */
   public HashMap<String, ArrayList<String>> getCorrection() {
     HashMap<String, ArrayList<String>> correction = new HashMap<>();
 
