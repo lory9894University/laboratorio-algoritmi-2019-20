2a1bd70078df344df9a978c3e9975b13ab47a0a2
“Lorenzo <dentis.lorenzo@educ.di.unito.it>
2020-03-28 16:57:28 +0100

    wrote my version of a counting sort for the vector of key-values

--- Esercizio3/exampleApp/main.c
+++ Esercizio3/exampleApp/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "hashmap.h"
 
+typedef struct _pair{
+  int key;
+  int value;
+  int occupied;
+}Pair;
+
+void mineCountingSort(Pair arr[], int range){
+  Pair hostArray[6321078];
+  int i;
+  Pair count[range + 1];
+  for (i = 0; i <range +1 ; ++i) {
+    count[i].occupied=0;
+  }
+  for (i = 0; i <6321078 ; ++i) {
+    count[arr[i].key].key=arr[i].key;
+    count[arr[i].key].value=arr[i].value;
+    count[arr[i].key].occupied=1;
+  }
+  for (int j=i=0; i <range +1 ; ++i) {
+    if (count[i].occupied){
+      arr[j].key=count[i].key;
+      arr[j].value=count[i].value;
+    }
+  }
+}
 int main(){
 
 }
