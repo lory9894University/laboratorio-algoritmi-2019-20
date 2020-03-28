2d0678797530afcbeb286c75b9ee66405962154b
“Lorenzo <dentis.lorenzo@educ.di.unito.it>
2020-03-28 16:56:53 +0100

    wrote some basic function (i don't know the hashing algorithm)

--- Esercizio3/include/hashmap.h
+++ Esercizio3/include/hashmap.h
@@ -2,4 +2,15 @@
 #ifndef ESERCIZIO3_HASHMAP_H
 #define ESERCIZIO3_HASHMAP_H
 
+typedef struct _hashmap * HashmapPtr;
+
+HashmapPtr newMap(int size);
+void freeMap(HashmapPtr  map);
+int isEmptyMap(HashmapPtr map);
+void cancelMap(HashmapPtr map);
+int countEntry(HashmapPtr map);
+void insertEntry(HashmapPtr map,void* key, void* value);
+void getValue(HashmapPtr map, void* key);
+void deleteValue(HashmapPtr map, void* key);
+void ** getKeys(HashmapPtr map);
 #endif //ESERCIZIO3_HASHMAP_H
--- Esercizio3/src/hashmap.c
+++ Esercizio3/src/hashmap.c
@@ -1,2 +1,88 @@
 #include "hashmap.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _entry * Link;
+typedef struct _entry{
+  void * key;
+  void * value;
+  Link next,previous;
+} Entry;
+
+typedef struct _hashmap{
+  Entry * map;
+  int entryNum,size;
+
+} Hashmap;
+
+/**
+ * function gets a key and hashes that value to get the position in the vetor**/
+int hashFunction(int key,Hashmap map){
+   int hash=0;
+
+   hash= hash%map.size;
+
+  return hash;
+ }
+
+Hashmap * newMap(int size){
+  Hashmap *map = malloc(sizeof(Hashmap));
+
+  map->entryNum=0;
+  map->size = size;
+  return map;
+}
+
+void cancelMap(Hashmap * map){
+  Link p;
+  //TODO: devo deallocare i puntatori a void delle entry?
+  for (int i = 0; i <map->size ; ++i) {
+    p=&map->map[i];
+    while(p->next!=NULL)
+      p=p->next;
+    while (p->previous!=NULL){
+      p=p->previous;
+      free(p->next);
+    }
+    free(p);
+  }
+  map->entryNum=0;
+}
+
+void freeMap(Hashmap * map){
+  cancelMap(map);
+  free(map->map);
+  free(map);
+}
+
+int isEmptyMap(Hashmap * map){
+  return map->entryNum==0;
+}
+
+int countEntry(Hashmap * map){
+  return map->entryNum;
+}
+
+void ** getKeys(HashmapPtr map){
+  void **keys;
+  int c=0;
+  Link p;
+
+  keys=malloc(sizeof(void*)*map->entryNum);
+  for (int i = 0; i <map->size ; ++i) {
+    if (map->map[i].value!=NULL){
+      p=&map->map[i];
+      while(p->next!=NULL)
+        p=p->next;
+      while (p->previous!=NULL){
+        p=p->previous;
+        keys[c]=p->next;
+        c++;
+      }
+      keys[c]=p;
+      c++;
+    }
+  }
+
+  return keys;
+}
