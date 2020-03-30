b67b5c623ac66b0aaa86bc9d94b9e612ed226859
“Lorenzo <dentis.lorenzo@educ.di.unito.it>
2020-03-30 10:28:57 +0200

    wrote csv reading to static Array

--- Esercizio3/exampleApp/main.c
+++ Esercizio3/exampleApp/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hashmap.h"
 
 typedef struct _pair{
@@ -7,6 +8,32 @@ typedef struct _pair{
   int value;
   int occupied;
 }Pair;
+
+/**reads from the file having path "filname" according to the csv format
+ * saves the data in a RecordCollection struct (struct containig an array of records and its size)**/
+Pair * csvReadingToArray(char * filename){
+  FILE *fPtr;
+  Pair *recordArray;
+  char textLine[1024];
+  int i=0;
+  char *token;
+
+  recordArray=malloc(sizeof(Pair)*6321078);
+  if((fPtr=fopen(filename,"r"))==NULL){
+    printf("file %s not found \n",filename);
+    exit(1);
+  }
+  while(fgets(textLine,1024,fPtr)!= NULL) {
+    token=strtok(textLine,",");
+    recordArray[i].key=atoi(token);
+    token=strtok(NULL,",");
+    recordArray[i].value=atoi(token);
+  }
+
+  fclose(fPtr);
+
+  return recordArray;
+}
 
 void mineCountingSort(Pair arr[], int range){
   Pair hostArray[6321078];
