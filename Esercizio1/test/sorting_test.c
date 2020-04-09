c489f8235e9a7b6723675a2023d53a6ebec9a143
“Lorenzo <dentis.lorenzo@educ.di.unito.it>
2020-04-09 10:35:21 +0200

    sublime left some tabs

--- Esercizio1/test/sorting_test.c
+++ Esercizio1/test/sorting_test.c
@@ -10,70 +10,70 @@ static int int_comparer(int *a, int *b) {
 }
 
 void empty_array(){
-	int ** empty;
-	insertion_sort((void*)empty,0,(cmpFunction)int_comparer);
-	TEST_PASS();
-	quick_sort((void*)empty,0,(cmpFunction)int_comparer);
-	TEST_PASS();
+  int ** empty;
+  insertion_sort((void*)empty,0,(cmpFunction)int_comparer);
+  TEST_PASS();
+  quick_sort((void*)empty,0,(cmpFunction)int_comparer);
+  TEST_PASS();
 }
 
 void casual_array(){
-	int ** array;
+  int ** array;
 
-	array=malloc(sizeof(int *)*5);
-	for (int i = 0; i < 5; ++i){
-		array[i]=malloc(sizeof(int));
-		array[i]=&numberList[i];
-	}
-	
-	insertion_sort((void*)array,5,(cmpFunction)int_comparer);
-	TEST_ASSERT_EQUAL(1,*array[0]);
-	TEST_ASSERT_EQUAL(2,*array[1]);
-	TEST_ASSERT_EQUAL(3,*array[2]);
-	TEST_ASSERT_EQUAL(5,*array[3]);
-	TEST_ASSERT_EQUAL(7,*array[4]);
+  array=malloc(sizeof(int *)*5);
+  for (int i = 0; i < 5; ++i){
+    array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
+  
+  insertion_sort((void*)array,5,(cmpFunction)int_comparer);
+  TEST_ASSERT_EQUAL(1,*array[0]);
+  TEST_ASSERT_EQUAL(2,*array[1]);
+  TEST_ASSERT_EQUAL(3,*array[2]);
+  TEST_ASSERT_EQUAL(5,*array[3]);
+  TEST_ASSERT_EQUAL(7,*array[4]);
 
-	array=malloc(sizeof(int *)*5);
-	for (int i = 0; i < 5; ++i){
-		array[i]=malloc(sizeof(int));
-		array[i]=&numberList[i];
-	}
-	
-	quick_sort((void*)array,5,(cmpFunction)int_comparer);
-	TEST_ASSERT_EQUAL(1,*array[0]);
-	TEST_ASSERT_EQUAL(2,*array[1]);
-	TEST_ASSERT_EQUAL(3,*array[2]);
-	TEST_ASSERT_EQUAL(5,*array[3]);
-	TEST_ASSERT_EQUAL(7,*array[4]);
+  array=malloc(sizeof(int *)*5);
+    for (int i = 0; i < 5; ++i){
+      array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
+  
+  quick_sort((void*)array,5,(cmpFunction)int_comparer);
+  TEST_ASSERT_EQUAL(1,*array[0]);
+  TEST_ASSERT_EQUAL(2,*array[1]);
+  TEST_ASSERT_EQUAL(3,*array[2]);
+  TEST_ASSERT_EQUAL(5,*array[3]);
+  TEST_ASSERT_EQUAL(7,*array[4]);
 }
 
 void equal_array(){
-	int ** array;
-	int numberList[5]={1,1,1,1,1};
+  int ** array;
+  int numberList[5]={1,1,1,1,1};
 
-	array=malloc(sizeof(int *)*5);
-	for (int i = 0; i < 5; ++i){
-  	array[i]=malloc(sizeof(int));
-		array[i]=&numberList[i];
-	}
+  array=malloc(sizeof(int *)*5);
+  for (int i = 0; i < 5; ++i){
+    array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
 
-	insertion_sort((void*)array,5,(cmpFunction)int_comparer);
-	for (int i = 0; i < 5; ++i)
-		{
-			TEST_ASSERT_EQUAL(1,*array[i]);
-		}	
+  insertion_sort((void*)array,5,(cmpFunction)int_comparer);
+  for (int i = 0; i < 5; ++i)
+    {
+      TEST_ASSERT_EQUAL(1,*array[i]);
+    }  
 
-	array=malloc(sizeof(int *)*5);
-	for (int i = 0; i < 5; ++i){
-		array[i]=malloc(sizeof(int));
-		array[i]=&numberList[i];
-	}
+  array=malloc(sizeof(int *)*5);
+  for (int i = 0; i < 5; ++i){
+    array[i]=malloc(sizeof(int));
+    array[i]=&numberList[i];
+  }
 
-	quick_sort((void*)array,5,(cmpFunction)int_comparer);
-	for (int i = 0; i < 5; ++i)
-		{
-			TEST_ASSERT_EQUAL(1,*array[i]);
-		}
+  quick_sort((void*)array,5,(cmpFunction)int_comparer);
+  for (int i = 0; i < 5; ++i)
+    {
+      TEST_ASSERT_EQUAL(1,*array[i]);
+    }
 }
 
 void ordered_array(){
@@ -141,13 +141,13 @@ void inverted_array(){
 }
 
 int main(){
-	UNITY_BEGIN();
+  UNITY_BEGIN();
 
-	RUN_TEST(empty_array);
-	RUN_TEST(casual_array);
-	RUN_TEST(equal_array);
+  RUN_TEST(empty_array);
+  RUN_TEST(casual_array);
+  RUN_TEST(equal_array);
   RUN_TEST(ordered_array);
   RUN_TEST(inverted_array);
 
-	UNITY_END();
+  UNITY_END();
 }
